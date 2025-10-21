044072c2283cf9a023eae3b22c4438c0453eb1ac
Mark <143808733+Mamarkx@users.noreply.github.com>
2025-10-21 12:15:36 +0800

    asd

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -21,7 +21,7 @@
     <!-- Swiper.js CDN -->
 <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-
+<link href="https://cdn.jsdelivr.net/npm/daisyui@5/daisyui.min.css" rel="stylesheet">
    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <style>
       body {
