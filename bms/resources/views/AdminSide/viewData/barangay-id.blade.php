5bdb6a069f4c11e3c5c3b293402f0679f001c130
Mark <143808733+Mamarkx@users.noreply.github.com>
2025-10-24 21:37:16 +0800

    asdsa

--- bms/resources/views/AdminSide/viewData/barangay-id.blade.php
+++ bms/resources/views/AdminSide/viewData/barangay-id.blade.php
@@ -88,7 +88,9 @@
                 </div>
 
                 <!-- Information Sections -->
-                <div class="px-4 sm:px-6 lg:px-8 py-8 space-y-8">
+                <!-- Information Sections -->
+                <div class="px-4 sm:px-6 lg:px-8 py-8 grid grid-cols-1 md:grid-cols-2 gap-5">
+
 
                     <!-- Personal Information -->
                     <div class="overflow-hidden">
