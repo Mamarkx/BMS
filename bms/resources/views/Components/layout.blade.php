aaf749dbdcc990b069c7160ba995231253c3216a
Mark <143808733+Mamarkx@users.noreply.github.com>
2025-10-25 14:09:07 +0800

    adasdasd

--- bms/resources/views/Components/layout.blade.php
+++ bms/resources/views/Components/layout.blade.php
@@ -575,7 +575,7 @@
   @auth
       <script>
           let idleTime = 0;
-          const maxIdleTime = 10; // 10 seconds
+          const maxIdleTime = 300; // 10 seconds
           const countdownSeconds = 5;
           let warningShown = false;
           let countdownInterval = null;
